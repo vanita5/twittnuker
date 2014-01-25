2b3400a52858023005fde07830c7529ce391b767
vanita5 <mail@vanita5.de>
2014-01-25 16:27:14 +0100

    updated secret code receiver

--- src/de/vanita5/twittnuker/receiver/SecretCodeBroadcastReceiver.java
+++ src/de/vanita5/twittnuker/receiver/SecretCodeBroadcastReceiver.java
@@ -25,17 +25,29 @@ package de.vanita5.twittnuker.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 
-import de.vanita5.twittnuker.activity.TestActivity;
+import de.vanita5.twittnuker.R;
+import de.vanita5.twittnuker.activity.SettingsActivity;
+import de.vanita5.twittnuker.constant.IntentConstants;
+import de.vanita5.twittnuker.fragment.SettingsDetailsFragment;
 
 
-public class TestBroadcastReceiver extends BroadcastReceiver {
+public class SecretCodeBroadcastReceiver extends BroadcastReceiver implements IntentConstants {
 
 	@Override
 	public void onReceive(final Context context, final Intent intent) {
-		final Intent test_intent = new Intent(context, TestActivity.class);
-		test_intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		context.startActivity(test_intent);
+        final Intent testIntent = new Intent(context, SettingsActivity.class);
+        final String cls = SettingsDetailsFragment.class.getName();
+        final String title = context.getString(R.string.hidden_settings);
+        final Bundle args = new Bundle();
+        args.putInt(EXTRA_RESID, R.xml.settings_hidden);
+        testIntent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT, cls);
+        testIntent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_ARGUMENTS, args);
+        testIntent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_TITLE, title);
+        testIntent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_SHORT_TITLE, title);
+        testIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(testIntent);
 	}
 
 }
