e74ccfdde587a5612ac2a0f455978695fed3be63
vanita5 <mail@vanita5.de>
2014-05-05 22:12:05 +0200

    Implemented handling of notification types

--- src/de/vanita5/twittnuker/service/PushService.java
+++ src/de/vanita5/twittnuker/service/PushService.java
@@ -9,27 +9,34 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import de.vanita5.twittnuker.Constants;
 import de.vanita5.twittnuker.R;
 import de.vanita5.twittnuker.activity.support.HomeActivity;
+import de.vanita5.twittnuker.app.TwittnukerApplication;
 import de.vanita5.twittnuker.model.AccountPreferences;
 import de.vanita5.twittnuker.model.PushNotificationContent;
 import de.vanita5.twittnuker.provider.TweetStore.PushNotifications;
 import de.vanita5.twittnuker.receiver.ClearNotificationReceiver;
 import de.vanita5.twittnuker.receiver.GCMReceiver;
 import de.vanita5.twittnuker.util.HtmlEscapeHelper;
+import de.vanita5.twittnuker.util.ImagePreloader;
 import de.vanita5.twittnuker.util.Utils;
 
 import static de.vanita5.twittnuker.util.Utils.getAccountIds;
@@ -77,19 +84,20 @@ public class PushService extends IntentService implements Constants {
 					Log.e(TAG, e.getMessage());
 					return;
 				}
-				PushNotificationContent notification = new PushNotificationContent();
-				notification.setAccountId(accountId);
-				notification.setFromUser(extras.getString("fromuser"));
-				notification.setType(extras.getString("type"));
-				notification.setMessage(extras.getString("msg"));
-				notification.setTimestamp(System.currentTimeMillis());
 
 				final AccountPreferences[] prefs = AccountPreferences.getPushEnabledPreferences(this, getAccountIds(this));
 				for (final AccountPreferences pref : prefs) {
 					if (pref.getAccountId() == accountId) {
 						//Push has been enabled for this account. Continue!
+						PushNotificationContent notification = new PushNotificationContent();
+						notification.setAccountId(accountId);
+						notification.setFromUser(extras.getString("fromuser"));
+						notification.setType(extras.getString("type"));
+						notification.setMessage(extras.getString("msg"));
+						notification.setTimestamp(System.currentTimeMillis());
+
 						cachePushNotification(notification);
-						buildNotification(notification, pref);
+						buildNotificationByType(notification, pref);
 						break;
 					}
 					//There is no such account with Push enabled...
@@ -100,37 +108,96 @@ public class PushService extends IntentService implements Constants {
 		GCMReceiver.completeWakefulIntent(intent);
 	}
 
-	private void buildNotification(final PushNotificationContent notification, final AccountPreferences pref) {
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+	private void buildNotificationByType(final PushNotificationContent notification, final AccountPreferences pref) {
+		final String type = notification.getType();
 		final int notificationType = pref.getMentionsNotificationType();
 		List<PushNotificationContent> pendingNotifications = getCachedPushNotifications(notification.getAccountId());
+		final int notificationCount = pendingNotifications.size();
+
+		String contentText = null;
+		String ticker = null;
+		//Bitmap icon; TODO
+
+		if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_MENTION.equals(type)) {
+			contentText = stripMentionText(notification.getMessage(),
+					getAccountScreenName(this, notification.getAccountId()));
+			ticker = notification.getMessage();
+		} else if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_RETWEET.equals(type)) {
+			contentText = getString(R.string.push_new_retweet_single)
+					+ ": " + notification.getMessage();
+			ticker = contentText; //TODO Should we really add the message to the ticker? We could
+		} else if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_FAVORITE.equals(type)) {
+			contentText = getString(R.string.push_new_favorite_single)
+					+ ": " + notification.getMessage();
+			ticker = contentText; //TODO Should we really add the message to the ticker?
+		} else if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_FOLLOWER.equals(type)) {
+			contentText = "@" + notification.getFromUser() + " " + getString(R.string.push_new_follower);
+			ticker = contentText;
+		}
+		if (contentText == null && ticker == null) return;
+		buildNotification(notification, pref, notificationType, notificationCount,
+				pendingNotifications, contentText, ticker, null);
+	}
+
+	private Spanned getInboxLineByType(final PushNotificationContent pendingNotification) {
+		final String type = pendingNotification.getType();
+		final String nameEscaped = HtmlEscapeHelper.escape("@" + pendingNotification.getFromUser());
+		final String textEscaped = HtmlEscapeHelper.escape(pendingNotification.getMessage());
+		if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_MENTION.equals(type)) {
+			return Html.fromHtml(String.format("<b>%s</b>: %s", nameEscaped, textEscaped));
+		} else if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_RETWEET.equals(type)) {
+			return Html.fromHtml(String.format("<b>%s " + getString(R.string.push_new_retweet) + "</b>: %s",
+					nameEscaped, textEscaped));
+		} else if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_FAVORITE.equals(type)) {
+			return Html.fromHtml(String.format("<b>%s " + getString(R.string.push_new_favorite) + "</b>: %s",
+					nameEscaped, textEscaped));
+		} else if (PushNotificationContent.PUSH_NOTIFICATION_TYPE_FOLLOWER.equals(type)) {
+			return Html.fromHtml(String.format("<b>%s</b> " + getString(R.string.push_new_follower),
+					nameEscaped));
+		}
+		return null;
+	}
+
+	private void buildNotification(final PushNotificationContent notification, final AccountPreferences pref,
+								   final int notificationType, final int notificationCount,
+								   List<PushNotificationContent> pendingNotifications, final String contentText,
+								   final String ticker, final Bitmap icon) {
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
 		builder.setContentTitle("@" + notification.getFromUser());
-		builder.setContentText(notification.getMessage());
-		builder.setTicker(notification.getMessage());
+		builder.setContentText(contentText);
+		builder.setTicker(ticker);
 		builder.setSmallIcon(R.drawable.ic_stat_twittnuker);
-		//TODO setLargeIcon
+		if (icon != null) builder.setLargeIcon(icon);
 		builder.setDeleteIntent(getDeleteIntent(notification.getAccountId()));
 		builder.setAutoCancel(true);
-
-		final int notificationCount = pendingNotifications.size();
-		builder.setContentInfo(String.valueOf(notificationCount));
-		builder.setWhen(System.currentTimeMillis());
+		builder.setWhen(notification.getTimestamp());
 
 		if (notificationCount > 1) {
 			NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-			inboxStyle.setBigContentTitle(notificationCount + " neue Interaktionen");
+			inboxStyle.setBigContentTitle(notificationCount + " " + getString(R.string.push_new_interactions));
 
 			for (PushNotificationContent pendingNotification : pendingNotifications) {
-				final String nameEscaped = HtmlEscapeHelper.escape("@" + pendingNotification.getFromUser());
-				final String textEscaped = HtmlEscapeHelper.escape(pendingNotification.getMessage());
-				inboxStyle.addLine(Html.fromHtml(String.format("<b>%s</b>: %s", nameEscaped, textEscaped)));
+				Spanned line = getInboxLineByType(pendingNotification);
+				if (line != null) inboxStyle.addLine(line);
 			}
 			inboxStyle.setSummaryText("@" + getAccountScreenName(this, notification.getAccountId()));
 			builder.setNumber(notificationCount);
 			builder.setStyle(inboxStyle);
 		} else {
-			//TODO Add big style? We need a status for this!
+			//final Intent replyIntent = new Intent(INTENT_ACTION_REPLY);
+			//replyIntent.setExtrasClassLoader(getClassLoader());
+			//replyIntent.putExtra(EXTRA_NOTIFICATION_ID, notificationType);
+			//replyIntent.putExtra(EXTRA_NOTIFICATION_ACCOUNT, notification.getAccountId());
+			////replyIntent.putExtra(EXTRA_STATUS, firstItem); TODO
+			//replyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			//builder.addAction(R.drawable.ic_action_reply, getString(R.string.reply),
+			//		PendingIntent.getActivity(this, 0, replyIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+			//final NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle(builder);
+			//bigTextStyle.bigText(stripMentionText(notification.getMessage(),
+			//		getAccountScreenName(this, notification.getAccountId())));
+			//bigTextStyle.setSummaryText("@" + getAccountScreenName(this, notification.getAccountId()));
+			//builder.setStyle(bigTextStyle);
 		}
 
 		int defaults = 0;
@@ -183,6 +250,26 @@ public class PushService extends IntentService implements Constants {
 		intent.setAction(INTENT_ACTION_PUSH_NOTIFICATION_CLEARED);
 		intent.putExtra(EXTRA_USER_ID, accountId);
 		return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+	}
+
+	private Bitmap getProfileImageForNotification(final String profileImageUrl) {
+		final TwittnukerApplication app = TwittnukerApplication.getInstance(this);
+		ImagePreloader imagePreloader = new ImagePreloader(this, app.getImageLoader());
+		final Resources res = getResources();
+		final int w = res.getDimensionPixelSize(android.R.dimen.notification_large_icon_width);
+		final int h = res.getDimensionPixelSize(android.R.dimen.notification_large_icon_height);
+		final File profileImageFile = imagePreloader.getCachedImageFile(profileImageUrl);
+		final Bitmap profile_image = profileImageFile != null && profileImageFile.isFile() ? BitmapFactory
+				.decodeFile(profileImageFile.getPath()) : null;
+		if (profile_image != null) return Bitmap.createScaledBitmap(profile_image, w, h, true);
+				else return null;
+	}
+
+	private static String stripMentionText(final String text, final String my_screen_name) {
+		if (text == null || my_screen_name == null) return text;
+		final String temp = "@" + my_screen_name + " ";
+		if (text.startsWith(temp)) return text.substring(temp.length());
+		return text;
 	}
 
 	private void cachePushNotification(final PushNotificationContent notification) {
