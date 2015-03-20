094c69014830213811809423d41edf5f95ce0640
vanita5 <mail@vanita5.de>
2015-03-20 18:35:28 +0100

    move launcher icon to mipmap

--- twittnuker/src/main/java/de/vanita5/twittnuker/preference/ThemePreviewPreference.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/preference/ThemePreviewPreference.java
@@ -157,7 +157,7 @@ public class ThemePreviewPreference extends Preference implements Constants, OnS
 			profileView.setBackgroundResource(0);
 			textView.setTextIsSelectable(false);
 
-			profileImageView.setImageResource(R.drawable.ic_launcher);
+            profileImageView.setImageResource(R.mipmap.ic_launcher);
 			nameView.setText(TWIDERE_PREVIEW_NAME);
 			screenNameView.setText("@" + TWIDERE_PREVIEW_SCREEN_NAME);
 
