92d370f0e627f893bd99333e6ec713c40d831070
vanita5 <mail@vanita5.de>
2015-04-24 16:08:29 +0200

    fix to clear unread count on scroll to top

--- twittnuker/src/main/java/de/vanita5/twittnuker/fragment/support/AbsStatusesFragment.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/fragment/support/AbsStatusesFragment.java
@@ -366,8 +366,11 @@ public abstract class AbsStatusesFragment<Data> extends AbsContentListFragment<A
 
     @Override
     public final boolean scrollToStart() {
-        saveReadPosition();
-        return super.scrollToStart();
+        final boolean result = super.scrollToStart();
+        if (result) {
+        	saveReadPosition();
+        }
+        return result;
     }
 
     @Override
