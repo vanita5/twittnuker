f3b70e7e4030eab76470f0f490721d7e5c05d8f2
vanita5 <mail@vanita5.de>
2015-09-12 21:31:04 +0200

    fixed crash when playing video

--- twittnuker/src/main/java/de/vanita5/twittnuker/util/VideoLoader.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/util/VideoLoader.java
@@ -42,133 +42,137 @@ import java.io.InputStream;
 
 public class VideoLoader {
 
-	private final Context mContext;
-	private final DiskCache mDiskCache;
-	private final ImageDownloader mImageDownloader;
-	private final AsyncTaskManager mTaskManager;
-	private final Bus mBus;
+    private final Context mContext;
+    private final DiskCache mDiskCache;
+    private final ImageDownloader mImageDownloader;
+    private final AsyncTaskManager mTaskManager;
+    private final Bus mBus;
 
-	public VideoLoader(Context context) {
-		final TwittnukerApplication app = TwittnukerApplication.getInstance(context);
-		mContext = context;
-		mDiskCache = app.getDiskCache();
+    public VideoLoader(Context context) {
+        final TwittnukerApplication app = TwittnukerApplication.getInstance(context);
+        mContext = context;
+        mDiskCache = app.getDiskCache();
         mImageDownloader = new TwidereImageDownloader(context, false);
-		mTaskManager = app.getAsyncTaskManager();
-		mBus = app.getMessageBus();
-	}
+        mTaskManager = app.getAsyncTaskManager();
+        mBus = app.getMessageBus();
+    }
 
-	public File getCachedVideoFile(final String url, boolean loadIfNotFound) {
-		if (url == null) return null;
-		final File cache = mDiskCache.get(url);
-		if (cache.exists())
-			return cache;
-		else if (loadIfNotFound) {
-			loadVideo(url, null);
-		}
-		return null;
-	}
+    public File getCachedVideoFile(final String url, boolean loadIfNotFound) {
+        if (url == null) return null;
+        final File cache = mDiskCache.get(url);
+        if (cache.exists())
+            return cache;
+        else if (loadIfNotFound) {
+            loadVideo(url, null);
+        }
+        return null;
+    }
 
     public boolean isLoading(String url) {
         return mTaskManager.hasRunningTasksForTag(url);
     }
 
 
-	public int loadVideo(String uri, VideoLoadingListener listener) {
-		if (mTaskManager.hasRunningTasksForTag(uri)) {
-			return 0;
-		}
-		return mTaskManager.add(new PreLoadVideoTask(mContext, this, listener, uri), true);
-	}
+    public int loadVideo(String uri, VideoLoadingListener listener) {
+        if (mTaskManager.hasRunningTasksForTag(uri)) {
+            return 0;
+        }
+        return mTaskManager.add(new PreLoadVideoTask(mContext, this, listener, uri), true);
+    }
 
-	private void notifyTaskFinish(String uri, boolean succeeded) {
-		mBus.post(new VideoLoadFinishedEvent());
-	}
+    private void notifyTaskFinish(String uri, boolean succeeded) {
+        mBus.post(new VideoLoadFinishedEvent());
+    }
 
-	public interface VideoLoadingListener {
+    public interface VideoLoadingListener {
 
-		void onVideoLoadingCancelled(String uri, VideoLoadingListener listener);
+        void onVideoLoadingCancelled(String uri, VideoLoadingListener listener);
 
-		void onVideoLoadingComplete(String uri, VideoLoadingListener listener, File file);
+        void onVideoLoadingComplete(String uri, VideoLoadingListener listener, File file);
 
-		void onVideoLoadingFailed(String uri, VideoLoadingListener listener, Exception e);
+        void onVideoLoadingFailed(String uri, VideoLoadingListener listener, Exception e);
 
-		void onVideoLoadingProgressUpdate(String uri, VideoLoadingListener listener, int current, int total);
+        void onVideoLoadingProgressUpdate(String uri, VideoLoadingListener listener, int current, int total);
 
-		void onVideoLoadingStarted(String uri, VideoLoadingListener listener);
-	}
+        void onVideoLoadingStarted(String uri, VideoLoadingListener listener);
+    }
 
     private static class PreLoadVideoTask extends ManagedAsyncTask<Object, Integer, SingleResponse<File>> implements IoUtils.CopyListener {
 
-		private final VideoLoader mPreLoader;
-		private final VideoLoadingListener mListener;
-		private final String mUri;
+        private final VideoLoader mPreLoader;
+        private final VideoLoadingListener mListener;
+        private final String mUri;
 
-		private PreLoadVideoTask(final Context context, final VideoLoader preLoader, VideoLoadingListener listener, final String uri) {
-			super(context, preLoader.mTaskManager, uri);
-			mPreLoader = preLoader;
-			mListener = listener;
-			mUri = uri;
-		}
+        private PreLoadVideoTask(final Context context, final VideoLoader preLoader, VideoLoadingListener listener, final String uri) {
+            super(context, preLoader.mTaskManager, uri);
+            mPreLoader = preLoader;
+            mListener = listener;
+            mUri = uri;
+        }
 
-		@Override
-		public boolean onBytesCopied(int current, int total) {
-			if (isCancelled()) return false;
-			publishProgress(current, total);
-			return true;
-		}
+        @Override
+        public boolean onBytesCopied(int current, int total) {
+            if (isCancelled()) return false;
+            publishProgress(current, total);
+            return true;
+        }
 
-		@Override
+        @Override
         protected SingleResponse<File> doInBackground(Object... params) {
-			final File file = mPreLoader.mDiskCache.get(mUri);
-			if (file.isFile() && file.length() > 0) return SingleResponse.getInstance(file);
-			try {
-				final InputStream is = mPreLoader.mImageDownloader.getStream(mUri, null);
-				mPreLoader.mDiskCache.save(mUri, is, this);
-				IoUtils.closeSilently(is);
-			} catch (IOException e) {
-				mPreLoader.mDiskCache.remove(mUri);
-				Log.w(LOGTAG, e);
-				return SingleResponse.getInstance(e);
-			}
-			return SingleResponse.getInstance(file);
-		}
+            final DiskCache diskCache = mPreLoader.mDiskCache;
+            final File cachedFile = diskCache.get(mUri);
+            if (cachedFile != null && cachedFile.isFile() && cachedFile.length() > 0)
+                return SingleResponse.getInstance(cachedFile);
+            InputStream is = null;
+            try {
+                is = mPreLoader.mImageDownloader.getStream(mUri, null);
+                diskCache.save(mUri, is, this);
+                return SingleResponse.getInstance(diskCache.get(mUri));
+            } catch (IOException e) {
+                diskCache.remove(mUri);
+                Log.w(LOGTAG, e);
+                return SingleResponse.getInstance(e);
+            } finally {
+                IoUtils.closeSilently(is);
+            }
+        }
 
-		@Override
-		protected void onProgressUpdate(Integer... values) {
-			super.onProgressUpdate(values);
-			if (mListener != null) {
-				mListener.onVideoLoadingProgressUpdate(mUri, mListener, values[0], values[1]);
-			}
-		}
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            if (mListener != null) {
+                mListener.onVideoLoadingProgressUpdate(mUri, mListener, values[0], values[1]);
+            }
+        }
 
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			if (mListener != null) {
-				mListener.onVideoLoadingStarted(mUri, mListener);
-			}
-		}
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mListener != null) {
+                mListener.onVideoLoadingStarted(mUri, mListener);
+            }
+        }
 
-		@Override
-		protected void onPostExecute(SingleResponse<File> result) {
-			super.onPostExecute(result);
-			if (mListener != null) {
-				if (result.hasData()) {
-					mListener.onVideoLoadingComplete(mUri, mListener, result.getData());
-				} else {
-					mListener.onVideoLoadingFailed(mUri, mListener, result.getException());
-				}
-			}
-			mPreLoader.notifyTaskFinish(mUri, result.hasData());
-		}
+        @Override
+        protected void onPostExecute(SingleResponse<File> result) {
+            super.onPostExecute(result);
+            if (mListener != null) {
+                if (result.hasData()) {
+                    mListener.onVideoLoadingComplete(mUri, mListener, result.getData());
+                } else {
+                    mListener.onVideoLoadingFailed(mUri, mListener, result.getException());
+                }
+            }
+            mPreLoader.notifyTaskFinish(mUri, result.hasData());
+        }
 
-		@Override
-		protected void onCancelled() {
-			super.onCancelled();
-			if (mListener != null) {
-				mListener.onVideoLoadingCancelled(mUri, mListener);
-			}
-		}
-	}
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            if (mListener != null) {
+                mListener.onVideoLoadingCancelled(mUri, mListener);
+            }
+        }
+    }
 
 }
