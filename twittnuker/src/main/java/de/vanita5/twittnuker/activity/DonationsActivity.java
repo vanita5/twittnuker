ca643fac1bcdcc1b09f2f8f11e26a9c99418e858
vanita5 <mail@vanita5.de>
2015-10-26 21:42:30 +0100

    in-app donation

--- twittnuker/src/main/java/de/vanita5/twittnuker/activity/DonationsActivity.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/activity/DonationsActivity.java
@@ -37,6 +37,17 @@ import de.vanita5.twittnuker.R;
 public class DonationsActivity extends FragmentActivity {
 
     /**
+     * Google
+     */
+    private static final String GOOGLE_PUBKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAs2KZ58y8Z56KchEP2iQHvuznrZAyDf9ULm+L0C2PZKcZjHGxC3XbXH9VC9qVV1GUcPJEIXht0VanUGYPHbCQDVnRPQuNyrF4rOLB5qLEh71IxnlK0OjnKGXRolSTldsZUhC1ja8n5MI0bi3r1oRduM0fDC4E+piIrfRZBjPm6p9OLckwgzz+rulYFErmQAoPhmUr4AvV3WgYNm0Lof+eLpZVpGfxqxOpmt3fMe30/nEnvLVHdOU1wNix9hq94uLrzHVLBuXTT7v99QnX/HB5dztnI54lGK7GvmwCTfrjcgdyf63D4+r1eF/E3Bx2kp/ZtezE0vWGda6bXgecdlJ/LQIDAQAB";
+    private static final String[] GOOGLE_CATALOG = new String[]{
+            "twittnuker.donate.1",
+            "twittnuker.donate.2",
+            "twittnuker.donate.5",
+            "twittnuker.donate.10",
+    };
+
+    /**
      * PayPal
      */
     private static final String PAYPAL_USER = "eliahwinkler@yahoo.de";
@@ -60,8 +71,12 @@ public class DonationsActivity extends FragmentActivity {
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         DonationsFragment donationsFragment;
-        donationsFragment = DonationsFragment.newInstance(BuildConfig.DEBUG, false, null, null, null, true, PAYPAL_USER,
-                PAYPAL_CURRENCY_CODE, getString(R.string.donation), true, FLATTR_PROJECT_URL, FLATTR_URL, false, null);
+        donationsFragment = DonationsFragment.newInstance(
+                BuildConfig.DEBUG,
+                true, GOOGLE_PUBKEY, GOOGLE_CATALOG, getResources().getStringArray(R.array.donation_google_catalog_values), //Google
+                true, PAYPAL_USER, PAYPAL_CURRENCY_CODE, getString(R.string.donation),                                      //Paypal
+                true, FLATTR_PROJECT_URL, FLATTR_URL,                                                                       //Flattr
+                false, null);                                                                                               //Bitcoin
 
 
         ft.replace(R.id.donations_activity_container, donationsFragment, "donationsFragment");
