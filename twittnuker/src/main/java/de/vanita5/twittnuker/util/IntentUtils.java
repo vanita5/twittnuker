08158f721dae866c018f2f673083e4b20c877d6d
vanita5 <mail@vanita5.de>
2016-04-24 09:46:10 +0200

    fixed status color

--- twittnuker/src/main/java/de/vanita5/twittnuker/util/IntentUtils.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/util/IntentUtils.java
@@ -254,7 +254,7 @@ public class IntentUtils implements Constants {
         intent.setData(testBuilder.build());
 
         final ComponentName componentName = intent.resolveActivity(context.getPackageManager());
-        if (componentName == null) return null;
+        if (componentName == null || componentName.getClassName() == null) return null;
         return componentName.getPackageName();
     }
 
