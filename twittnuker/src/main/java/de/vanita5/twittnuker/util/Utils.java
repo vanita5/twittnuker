ce430eea3e96dfcf05c5b4eba9edf43516248396
vanita5 <mail@vanita5.de>
2016-06-20 20:47:40 +0200

    Add title customizable sendPebbleNotification method

--- twittnuker/src/main/java/de/vanita5/twittnuker/util/Utils.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/util/Utils.java
@@ -2180,12 +2180,33 @@ public final class Utils implements Constants {
      * @param message String
      */
     public static void sendPebbleNotification(final Context context, final String message) {
+        sendPebbleNotification(context, null, message);
+    }
+
+    /**
+     * Send Notifications to Pebble smartwatches
+     *
+     * @param context Context
+     * @param title String
+     * @param message String
+     */
+    public static void sendPebbleNotification(final Context context, final String title, final String message)
+    {
+        String appName;
+
+        if ( title == null)
+        {
+            appName = context.getString(R.string.app_name);
+        }
+        else
+        {
+            appName = context.getString(R.string.app_name) + " - " + title;
+        }
+
         if (context == null || TextUtils.isEmpty(message)) return;
         final SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
 
         if (prefs.getBoolean(KEY_PEBBLE_NOTIFICATIONS, false)) {
-
-            final String appName = context.getString(R.string.app_name);
 
             final List<PebbleMessage> messages = new ArrayList<>();
             messages.add(new PebbleMessage(appName, message));
