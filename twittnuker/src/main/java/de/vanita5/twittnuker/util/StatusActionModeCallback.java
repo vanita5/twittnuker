4f57688738b0730b0ebf9bf2a793e25359e2369a
vanita5 <mail@vanita5.de>
2016-06-20 20:50:44 +0200

    added share link

--- twittnuker/src/main/java/de/vanita5/twittnuker/util/StatusActionModeCallback.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/util/StatusActionModeCallback.java
@@ -23,7 +23,7 @@
 package de.vanita5.twittnuker.util;
 
 import android.content.Context;
-import android.support.v4.view.MenuItemCompat;
+import android.content.Intent;
 import android.text.SpannableString;
 import android.text.style.URLSpan;
 import android.view.ActionMode;
@@ -35,50 +35,62 @@ import android.widget.TextView;
 import de.vanita5.twittnuker.R;
 
 public class StatusActionModeCallback implements ActionMode.Callback {
-	private final TextView textView;
-	private final Context context;
+    private final TextView textView;
+    private final Context context;
 
-	public StatusActionModeCallback(TextView textView, Context context) {
-		this.textView = textView;
-		this.context = context;
-	}
+    public StatusActionModeCallback(TextView textView, Context context) {
+        this.textView = textView;
+        this.context = context;
+    }
 
-	@Override
-	public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-		mode.getMenuInflater().inflate(R.menu.action_status_text_selection, menu);
-		mode.setTitle(android.R.string.selectTextMode);
-		return true;
-	}
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        mode.getMenuInflater().inflate(R.menu.action_status_text_selection, menu);
+        mode.setTitle(android.R.string.selectTextMode);
+        return true;
+    }
 
-	@Override
-	public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-		final int start = textView.getSelectionStart(), end = textView.getSelectionEnd();
-		final SpannableString string = SpannableString.valueOf(textView.getText());
-		final URLSpan[] spans = string.getSpans(start, end, URLSpan.class);
-		final boolean avail = spans.length == 1 && URLUtil.isValidUrl(spans[0].getURL());
-		MenuUtils.setMenuItemAvailability(menu, android.R.id.copyUrl, avail);
-		MenuUtils.setMenuItemShowAsActionFlags(menu, android.R.id.copyUrl, MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
-		return true;
-	}
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        final int start = textView.getSelectionStart(), end = textView.getSelectionEnd();
+        final SpannableString string = SpannableString.valueOf(textView.getText());
+        final URLSpan[] spans = string.getSpans(start, end, URLSpan.class);
+        final boolean selectingLink = spans.length == 1 && URLUtil.isValidUrl(spans[0].getURL());
+        MenuUtils.setMenuItemAvailability(menu, R.id.copy_url, selectingLink);
+        MenuUtils.setMenuItemAvailability(menu, R.id.share_url, selectingLink);
+        return true;
+    }
 
-	@Override
-	public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.copyUrl: {
-				final int start = textView.getSelectionStart(), end = textView.getSelectionEnd();
-				final SpannableString string = SpannableString.valueOf(textView.getText());
-				final URLSpan[] spans = string.getSpans(start, end, URLSpan.class);
-				if (spans.length != 1) return true;
-				ClipboardUtils.setText(context, spans[0].getURL());
-				mode.finish();
-				return true;
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.copy_url: {
+                final int start = textView.getSelectionStart(), end = textView.getSelectionEnd();
+                final SpannableString string = SpannableString.valueOf(textView.getText());
+                final URLSpan[] spans = string.getSpans(start, end, URLSpan.class);
+                if (spans.length != 1) return true;
+                ClipboardUtils.setText(context, spans[0].getURL());
+                mode.finish();
+                return true;
+            }
+            case R.id.share_url: {
+                final int start = textView.getSelectionStart(), end = textView.getSelectionEnd();
+                final SpannableString string = SpannableString.valueOf(textView.getText());
+                final URLSpan[] spans = string.getSpans(start, end, URLSpan.class);
+                if (spans.length != 1) return true;
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, spans[0].getURL());
+                context.startActivity(Intent.createChooser(shareIntent, context.getString(R.string.share_link)));
+                mode.finish();
+                return true;
+            }
+        }
+        return false;
+    }
 
-	@Override
-	public void onDestroyActionMode(ActionMode mode) {
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
 
-	}
+    }
 }
