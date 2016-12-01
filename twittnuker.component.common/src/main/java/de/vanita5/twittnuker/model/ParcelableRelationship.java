14fd74947a9f86e77756c2245ed79b3badc68a48
vanita5 <mail@vanit.as>
2016-12-01 13:56:43 +0100

    fix build, add missing annotation

--- twittnuker.component.common/src/main/java/de/vanita5/twittnuker/model/ParcelableRelationship.java
+++ twittnuker.component.common/src/main/java/de/vanita5/twittnuker/model/ParcelableRelationship.java
@@ -33,6 +33,7 @@ import de.vanita5.twittnuker.model.util.UserKeyCursorFieldConverter;
 import de.vanita5.twittnuker.provider.TwidereDataStore;
 import de.vanita5.twittnuker.provider.TwidereDataStore.CachedRelationships;
 
+@ParcelablePlease
 @CursorObject(valuesCreator = true, tableInfo = true)
 public class ParcelableRelationship implements Parcelable {
 
