d3883ef081a9e0a33dc14ec512ef82bc505ea741
vanita5 <mail@vanit.as>
2016-12-05 22:05:27 +0100

    added color border to account icon

--- twittnuker/src/main/java/de/vanita5/twittnuker/model/util/AccountUtils.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/model/util/AccountUtils.java
@@ -97,6 +97,8 @@ public class AccountUtils {
         details.activated = AccountExtensionsKt.isActivated(account, am);
         details.type = AccountExtensionsKt.getAccountType(account, am);
         details.credentials_type = AccountExtensionsKt.getCredentialsType(account, am);
+
+        details.user.color = details.color;
         return details;
     }
 
