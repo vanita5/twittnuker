5b36e24b731e84f5059d233532b69053b2573853
vanita5 <mail@vanit.as>
2017-02-07 12:18:55 +0100

    updated JobServiceSupport.java license to unlicense

--- twittnuker/src/main/java/de/vanita5/twittnuker/util/support/JobServiceSupport.java
+++ twittnuker/src/main/java/de/vanita5/twittnuker/util/support/JobServiceSupport.java
@@ -1,25 +1,29 @@
 /*
- *  Twittnuker - Twitter client for Android
+ * This is free and unencumbered software released into the public domain.
  *
- *  Copyright (C) 2013-2017 vanita5 <mail@vanit.as>
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
  *
- *  This program incorporates a modified version of Twidere.
- *  Copyright (C) 2012-2017 Mariotaku Lee <mariotaku.lee@gmail.com>
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * For more information, please refer to <http://unlicense.org>
  */
-
 package de.vanita5.twittnuker.util.support;
 
 import android.annotation.TargetApi;
@@ -30,7 +34,9 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-
+/**
+ * A fix to https://code.google.com/p/android/issues/detail?id=104302
+ */
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class JobServiceSupport {
     public static boolean handleStopJob(JobParameters params, boolean reschedule) {
@@ -54,7 +60,6 @@ public class JobServiceSupport {
                 acknowledgeStopMessageMethod.invoke(callbackCls, params.getJobId(), reschedule);
                 return true;
             } catch (NullPointerException npe) {
-                // https://code.google.com/p/android/issues/detail?id=104302
                 // Treat as handled
                 return true;
             }
