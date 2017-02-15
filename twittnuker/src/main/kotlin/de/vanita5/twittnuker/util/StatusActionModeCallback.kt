36fc53d2d3b8e916f23e199b56bf4abc018f6baa
vanita5 <mail@vanit.as>
2017-02-15 12:42:45 +0100

    fixed some NPE

--- twittnuker/src/main/kotlin/de/vanita5/twittnuker/util/StatusActionModeCallback.kt
+++ twittnuker/src/main/kotlin/de/vanita5/twittnuker/util/StatusActionModeCallback.kt
@@ -28,6 +28,7 @@ import android.text.SpannableString
 import android.text.style.URLSpan
 import android.view.ActionMode
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.webkit.URLUtil
 import android.widget.TextView
@@ -37,7 +38,8 @@ import de.vanita5.twittnuker.R
 class StatusActionModeCallback(private val textView: TextView, private val context: Context) : ActionMode.Callback {
 
     override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
-        mode.menuInflater.inflate(R.menu.action_status_text_selection, menu)
+        val inflater = mode.menuInflater ?: MenuInflater(context)
+        inflater.inflate(R.menu.action_status_text_selection, menu)
         mode.setTitle(android.R.string.selectTextMode)
         return true
     }
