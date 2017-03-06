252f1ac097ee29e548ca514589b656d3857bda3f
vanita5 <mail@vanit.as>
2017-03-06 22:29:53 +0100

    fixed some compile warnings

--- twittnuker/src/main/kotlin/de/vanita5/twittnuker/adapter/ParcelableUserListsAdapter.kt
+++ twittnuker/src/main/kotlin/de/vanita5/twittnuker/adapter/ParcelableUserListsAdapter.kt
@@ -66,10 +66,10 @@ class ParcelableUserListsAdapter(
     override fun getItemCount(): Int {
         val position = loadMoreIndicatorPosition
         var count = userListsCount
-        if (position and ILoadMoreSupportAdapter.START !== 0L) {
+        if (position and ILoadMoreSupportAdapter.START != 0L) {
             count++
         }
-        if (position and ILoadMoreSupportAdapter.END !== 0L) {
+        if (position and ILoadMoreSupportAdapter.END != 0L) {
             count++
         }
         return count
@@ -113,7 +113,7 @@ class ParcelableUserListsAdapter(
     }
 
     override fun getItemViewType(position: Int): Int {
-        if (loadMoreIndicatorPosition and ILoadMoreSupportAdapter.START !== 0L && position == 0) {
+        if (loadMoreIndicatorPosition and ILoadMoreSupportAdapter.START != 0L && position == 0) {
             return ITEM_VIEW_TYPE_LOAD_INDICATOR
         }
         if (position == userListsCount) {
