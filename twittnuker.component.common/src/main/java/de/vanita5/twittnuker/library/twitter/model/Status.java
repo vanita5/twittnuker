5cc387325b6a47c34fce3aed2c133e72a1aa19fb
vanita5 <mail@vanit.as>
2017-03-16 17:26:58 +0100

    added more precise timestamp_ms for streaming status

--- twittnuker.component.common/src/main/java/de/vanita5/twittnuker/library/twitter/model/Status.java
+++ twittnuker.component.common/src/main/java/de/vanita5/twittnuker/library/twitter/model/Status.java
@@ -208,6 +208,9 @@ public class Status extends TwitterResponseObject implements Comparable<Status>,
     @JsonField(name = "uri")
     String uri;
 
+    @JsonField(name = "timestamp_ms")
+    long timestampMs = -1;
+
     @ParcelableNoThanks
     private transient long sortId = -1;
 
@@ -268,6 +271,9 @@ public class Status extends TwitterResponseObject implements Comparable<Status>,
      * UTC time when this Tweet was created.
      */
     public Date getCreatedAt() {
+        if (timestampMs != -1) {
+            return new Date(timestampMs);
+        }
         return createdAt;
     }
 
